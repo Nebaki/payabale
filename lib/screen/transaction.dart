2dac8e7f0dac49fd7e3f0bc3a73d2f547e0fc7b0
Nebaki <112889837+Nebaki@users.noreply.github.com>
2023-02-10 00:59:44 +0300

    Update transaction.dart

--- lib/screen/transaction.dart
+++ lib/screen/transaction.dart
@@ -19,11 +19,11 @@ class _TransactionState extends State<Transaction> {
       appBar: AppBar(
         backgroundColor: primaryColor,
         elevation: 0,
-        title: Text('Hello, Andi'),
+        title: const Text('Hello, Andi'),
         centerTitle: true,
         actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
+          const Padding(
+            padding: EdgeInsets.only(right: 8.0),
             child: CircleAvatar(
               backgroundImage: AssetImage(
                 'assets/images/Neba.jpeg',
@@ -40,8 +40,8 @@ class _TransactionState extends State<Transaction> {
           ),
           Expanded(
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30.0),
@@ -51,7 +51,7 @@ class _TransactionState extends State<Transaction> {
               child: Column(children: [
                 Row(
                   children: [
-                    Text(
+                    const Text(
                       "Transaction",
                       style: TextStyle(
                           color: primaryColor,
@@ -59,25 +59,24 @@ class _TransactionState extends State<Transaction> {
                           fontSize: 20),
                     ),
                     Expanded(child: Container()),
-                    Text(
+                    const Text(
                       "sort by",
                       style: TextStyle(fontSize: 14),
                     ),
-                    Text(
+                    const Text(
                       "Date",
                       style: TextStyle(
                           color: primaryColor,
                           fontWeight: FontWeight.bold,
                           fontSize: 17),
                     ),
-                    Icon(
+                    const Icon(
                       Icons.arrow_drop_down_rounded,
                       color: primaryColor,
                     )
                   ],
                 ),
-                Text('Today'),
-                
+                const Text('Today'),
               ]),
             ),
           ),
